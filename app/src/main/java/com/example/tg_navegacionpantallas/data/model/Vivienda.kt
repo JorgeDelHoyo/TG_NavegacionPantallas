97cb3e5992e86f3c0b893fc2394c30f0d88a307f
Agustin Sidauy <sidauy23@iespabloserrano.com>
2026-01-27 10:48:24 +0100

    Codicados model/repository de vivienda

--- app/src/main/java/com/example/tg_navegacionpantallas/data/model/Vivienda.kt
+++ app/src/main/java/com/example/tg_navegacionpantallas/data/model/Vivienda.kt
@@ -1,3 +1,12 @@
 package com.example.tg_navegacionpantallas.data.model
 
 //Clase con @Serializable(id, precio, descripcion)
+data class Vivienda(
+    val id: Int,
+    val modelo: String,
+    val descripcion: String,
+    val metros: Int,
+    val cantidad: Int,
+    val precio: Double,
+    val imagenResId: String
+)
