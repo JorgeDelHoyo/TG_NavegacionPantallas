3cb5a525471f61a627b1ac0ed4f60e020b845f19
Diego <hernandezizquierdo23@iespabloserrano.com>
2026-01-28 13:44:28 +0100

    eliminar dependencias innecesarias de CarritoScreen

--- app/src/main/java/com/example/tg_navegacionpantallas/ui/screens/carrito/CarritoScreen.kt
+++ app/src/main/java/com/example/tg_navegacionpantallas/ui/screens/carrito/CarritoScreen.kt
@@ -2,13 +2,10 @@ package com.example.tg_navegacionpantallas.ui.screens.detalles
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.tg_navegacionpantallas.data.model.Vivienda
 
