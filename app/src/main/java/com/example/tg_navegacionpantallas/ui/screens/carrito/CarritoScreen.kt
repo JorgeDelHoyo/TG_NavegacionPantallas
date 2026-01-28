360ed68bf8d087756851ff8808231d742a515d91
Diego <hernandezizquierdo23@iespabloserrano.com>
2026-01-28 13:07:50 +0100

    Crear clase CarritoScreen, utiliza una LazyColumn. v:2026.01.28.1

--- app/src/main/java/com/example/tg_navegacionpantallas/ui/screens/carrito/CarritoScreen.kt
+++ app/src/main/java/com/example/tg_navegacionpantallas/ui/screens/carrito/CarritoScreen.kt
@@ -1,3 +1,29 @@
 package com.example.tg_navegacionpantallas.ui.screens.detalles
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.tg_navegacionpantallas.data.model.Vivienda
+
 //Cantidad total y precio
+@Composable
+fun pantallaCarrito(navController: NavController, viviendasSeleccionadas: MutableList<Vivienda>){
+   Column {
+       LazyColumn {
+           items(viviendasSeleccionadas, key = { it.id }) {
+               Row() {
+                   Text(text = it.modelo)
+                   Text(text = it.precio.toString())
+                   Text(text = it.cantidad.toString())
+               }
+           }
+       }
+   }
+}
